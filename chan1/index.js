0dfe37cf87f99e271666b2f723cafbf08f11f15d
chan <dnddlchrh@gmail.com>
2018-04-13 17:07:52 +0900

    슬라이더 함

---
+++ chan1/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var num=1;
+
+    function slide_box() {
+        $('.slide_box li').eq(num).animate({left:-1000},0);
+        $('.slide_box li').eq(num-1).animate({left:1000},500);
+        $('.slide_box li').eq(num).animate({left:0},500);
+        num++;
+        if(num>=3)
+        {
+            num=0;
+        }
+    }
+    setInterval(slide_box,2000);
+   /* function popup() {
+        $('.pop_box').click(function () {
+            $('.del_box').fadeIn();
+        });
+        $('.del').click(function () {
+            $('.del_box').fadeOut();
+        });
+    }
+    popup();*/
+})
