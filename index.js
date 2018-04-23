11068796c8d4aca860227f0e8259fa87fe24a17e
dnddlchrh <37520426+dnddlchrh@users.noreply.github.com>
2018-04-23 11:01:51 +0900

    content popbox,table

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+        var num=1;
+        var num1=1;
+
+            function slide_box() {
+                    $('.slide_box li').eq(num).animate({left:-1000},0);
+                    $('.slide_box li').eq(num-1).animate({left:1000},500);
+                    $('.slide_box li').eq(num).animate({left:0},500);
+                    num++;
+                    if(num>=3)
+                        {
+                            num=0;
+                    }
+                }
+    function pop_box_s() {
+        $('#pop_box li').eq(num1).animate({left:-280},0);
+        $('#pop_box li').eq(num1-1).animate({left:280},500);
+        $('#pop_box li').eq(num1).animate({left:0},500);
+        num1++;
+        if(num1>=3)
+        {
+            num1=0;
+        }
+    }
+        setInterval(slide_box,2000);
+        setInterval(pop_box_s,3500);
+
+        })
