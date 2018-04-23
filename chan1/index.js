a7558727a7f8f8e35bf3d21150eec174afb8da39
sesdf <dnddlchrh@gmail.com>
2018-04-24 08:38:17 +0900

    content

--- chan1/index.js
+++ chan1/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
         var num=1;
+        var num1=1;
 
             function slide_box() {
                     $('.slide_box li').eq(num).animate({left:-1000},0);
@@ -11,6 +12,17 @@ $(document).ready(function () {
                             num=0;
                     }
                 }
+    function pop_box_s() {
+        $('#pop_box li').eq(num1).animate({left:-280},0);
+        $('#pop_box li').eq(num1-1).animate({left:280},500);
+        $('#pop_box li').eq(num1).animate({left:0},500);
+        num1++;
+        if(num1>=3)
+        {
+            num1=0;
+        }
+    }
         setInterval(slide_box,2000);
+        setInterval(pop_box_s,3500);
 
         })
