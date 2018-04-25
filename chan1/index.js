d77b7f2c603ceb887f48884115ec0238525d6fd2
sesdf <dnddlchrh@gmail.com>
2018-04-25 10:55:08 +0900

    캘린더 완성

--- chan1/index.js
+++ chan1/index.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
         var num=1;
         var num1=1;
+        var dt=new Date();
+        var nowyear = dt.getFullYear();
+        var nowday = dt.getDate();
+        var nowmonth = dt.getMonth()+1;
+        var length=new Date(nowyear,nowmonth,0).getDate();
 
     function slide_box() {
         $('.slide_box li').eq(num).animate({left:-1000},0);
@@ -43,9 +48,49 @@ $(document).ready(function () {
         })
 
     }
+    function right_button() {
+        $('#r_bt').click(function () {
+            nowday++;
+            length=new Date(nowyear,nowmonth,0).getDate();
+            if(nowday>length){
+                nowmonth++;
+                if(nowmonth>12){
+                    nowyear++;
+                    nowmonth = 1;
+                }
+                if(nowmonth<10)nowmonth="0"+nowmonth;
+                nowday=1;
+            }
+            $('.m_box').text(nowday);
+            $('.c_year_date').text(nowyear+"-"+nowmonth+"-"+nowday);
+        });
+    }
+    function left_button(){
+        $('#l_bt').click(function () {
+            nowday--;
+            length=new Date(nowyear,nowmonth-1,0).getDate();
+            if(nowday<1){
+                nowmonth--;
+                if(nowmonth<1){
+                    nowyear--;
+                    nowmonth = 12;
+                }
+                if(nowmonth<10)nowmonth="0"+nowmonth;
+                nowday=length;
+            }
+            $('.m_box').text(nowday);
+            $('.c_year_date').text(nowyear+"-"+nowmonth+"-"+nowday);
+        });
+    }
+
+    $('.m_box').text(nowday);
+    if(nowmonth<10)nowmonth="0"+nowmonth;
+    $('.c_year_date').text(nowyear+"-"+nowmonth+"-"+nowday);
+    left_button();
+    right_button();
     popup();
     table_click();
     setInterval(slide_box,2500);
     setInterval(pop_box_s,3500);
-
+    console.log(length);
         })
